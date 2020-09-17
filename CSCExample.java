ac25fe46c629a8bc397e46d7ccda171b1590777e
Nawabyousufi <kashthuyguna@gmail.com>
2020-09-18 04:43:39 +0530

    add geters

---
+++ CSCExample.java
@@ -0,0 +1,22 @@
+public class CSCExample{
+	
+public int getQuestionNo(int questionNo){
+	questionNo=questionNo;
+}
+public String getCategory(String category){
+	category=category;
+}
+public Strign getText(String text){
+	text=text;
+}
+public int addQuestion(CSCExample Qu){
+	
+}
+public int removeQuestion(int quId){
+	
+}
+public int editQuestion(int quId){
+	
+}
+	
+}
