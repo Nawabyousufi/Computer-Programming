714bcae959bb143c78e794ce09d2e36e86c3f19f
Nawabyousufi <kashthuyguna@gmail.com>
2020-09-18 04:55:48 +0530

    adding some extra code

--- CSCExamples.java
+++ CSCExamples.java
@@ -2,5 +2,52 @@ public class CSCExample{
 int questionNo;
 String category;
 String text;	
+public void setQuestionNo(){
+	
+}
+public void setCategory(){
+	
+}
+public void setText(){
+	
+}
+public int getQuestionNo(int questionNo){
+	questionNo=questionNo;
+}
+public String getCategory(String category){
+	category=category;
+
+}
+public Strign getText(String text){
+	text=text;
+}
+public int addQuestion(CSCExample Qu){
+	
+}
+public int removeQuestion(int quId){
+	
+}
+public int editQuestion(int quId){
+	
+}
+public int getQuestionNo(int questionNo){
+
+}
+public String getCategory(String category){
+
+}
+public Strign getText(String text){
+	
+}
+public int addQuestion(CSCExample Qu){
+	
+}
+public int removeQuestion(int quId){
+	
+}
+public int editQuestion(int quId){
+	
+}
+
 }
 
