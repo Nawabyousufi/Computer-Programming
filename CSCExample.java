794eb8a3336a99ebbffe6148efb15cc95f540d89
Nawabyousufi <kashthuyguna@gmail.com>
2020-09-18 05:22:47 +0530

    Implement the Master method

--- CSCExample.java
+++ CSCExample.java
@@ -1,11 +1,21 @@
 public CSCEXamples{
 	
-public void setQuestionNo(){
-	
+public void setQuestionNo(int i){
+	questionNo=i;
 }
-public void setCategory(){
-	
+public void setCategory(String i){
+	category=i;
 }
 public void setText(){
-	
+	text=i;
+}
+public int getQuestionNO(){
+	return questionNo;
+}
+public int getCategory(){
+	return category;
+}
+public int getText(){
+	return text;
+}
 }
