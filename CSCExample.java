51c920351869d640e284cb10caaaee5b26f709d9
Nawabyousufi <kashthuyguna@gmail.com>
2020-09-18 05:40:45 +0530

    error solved

--- CSCExample.java
+++ CSCExample.java
@@ -1,21 +1,21 @@
-public CSCEXamples{
+public CSCEXample{
 	
 public void setQuestionNo(int i){
 	questionNo=i;
-}
+  }
 public void setCategory(String i){
 	category=i;
-}
+  }
 public void setText(){
 	text=i;
-}
+  }
 public int getQuestionNO(){
 	return questionNo;
-}
+ }
 public int getCategory(){
 	return category;
-}
+ }
 public int getText(){
 	return text;
-}
+  }
 }
